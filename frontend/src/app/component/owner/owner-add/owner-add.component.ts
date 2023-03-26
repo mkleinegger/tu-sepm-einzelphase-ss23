332716d7a053f1b67fc7b69adf89e56bdfbc9915
mkleinegger@gmail.com <mkleinegger@gmail.com>
2023-03-26 23:35:16 +0200

    [US7, TS26] adding errorhandling to create owner

--- frontend/src/app/component/owner/owner-add/owner-add.component.ts
+++ frontend/src/app/component/owner/owner-add/owner-add.component.ts
@@ -1,19 +1,17 @@
 import { Component } from '@angular/core';
-import {NgForm, NgModel} from '@angular/forms';
-import {ActivatedRoute, Router} from '@angular/router';
-import {ToastrService} from 'ngx-toastr';
-import {Observable, of} from 'rxjs';
-import {Owner} from 'src/app/dto/owner';
-import {OwnerService} from 'src/app/service/owner.service';
+import { NgForm, NgModel } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Observable, of } from 'rxjs';
+import { Owner } from 'src/app/dto/owner';
+import { OwnerService } from 'src/app/service/owner.service';
 
 @Component({
   selector: 'app-owner-add',
   templateUrl: './owner-add.component.html',
-  styleUrls: ['./owner-add.component.scss']
+  styleUrls: ['./owner-add.component.scss'],
 })
-
 export class OwnerAddComponent {
-
   owner: Owner = {
     firstName: '',
     lastName: '',
@@ -23,9 +21,8 @@ export class OwnerAddComponent {
   constructor(
     private service: OwnerService,
     private router: Router,
-    private notification: ToastrService,
-  ) {
-  }
+    private notification: ToastrService
+  ) {}
 
   public dynamicCssClassesForInput(input: NgModel): any {
     return {
@@ -39,20 +36,40 @@ export class OwnerAddComponent {
   public onSubmit(form: NgForm): void {
     console.log('is form valid?', form.valid, this.owner);
     if (form.valid) {
-      let observable: Observable<Owner>;
-
-      observable = this.service.create(this.owner);
+      const observable: Observable<Owner> = this.service.create(this.owner);
       observable.subscribe({
-        next: data => {
-          this.notification.success(`Owner ${this.owner.firstName} ${this.owner.lastName} successfully created.`);
+        next: () => {
+          this.notification.success(
+            `Owner ${this.owner.firstName} ${this.owner.lastName} successfully created.`
+          );
           this.router.navigate(['/owners']);
         },
-        error: error => {
-          console.error('Error creating horse', error);
-          // TODO show an error message to the user. Include and sensibly present the info from the backend!
-        }
+        error: (error) => {
+          console.error('Error creating owner', error);
+          this.errorHandler(error);
+        },
       });
     }
   }
 
+  private errorHandler(
+    error: any,
+    action: string | undefined = undefined
+  ): void {
+    if (error.status === 0) {
+      this.notification.error('Backend is not reachable');
+    } else if (error.status === 404) {
+      this.notification.error(
+        `Could not ${action} horse - ` + error.error.error
+      );
+    } else if (error.status === 409 || error.status === 422) {
+      for (const err of error.error.errors) {
+        this.notification.error(err);
+      }
+    } else if (error.status >= 500) {
+      this.notification.error(
+        error.error.error + ': Something unexpected happend!'
+      );
+    }
+  }
 }
