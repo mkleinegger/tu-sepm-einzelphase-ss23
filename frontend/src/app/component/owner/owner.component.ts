5fee994f5db4d107ae97a853edc51d135ec46679
mkleinegger@gmail.com <mkleinegger@gmail.com>
2023-03-26 23:35:44 +0200

    [US8] small adjustments for layout concerning ownerslist

--- frontend/src/app/component/owner/owner.component.ts
+++ frontend/src/app/component/owner/owner.component.ts
@@ -1,41 +1,55 @@
-import { Component } from '@angular/core';
-import {ToastrService} from 'ngx-toastr';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { OwnerService } from './../../service/owner.service';
-import {Owner} from '../../dto/owner';
-import {Observable} from 'rxjs';
-
+import { Owner } from '../../dto/owner';
 
 @Component({
   selector: 'app-owner',
   templateUrl: './owner.component.html',
-  styleUrls: ['./owner.component.scss']
+  styleUrls: ['./owner.component.scss'],
 })
-export class OwnerComponent {
+export class OwnerComponent implements OnInit {
   owners: Owner[] = [];
 
   constructor(
     private service: OwnerService,
-    private notification: ToastrService,
-  ) { }
+    private notification: ToastrService
+  ) {}
 
   ngOnInit(): void {
     this.reloadOwners();
   }
-  
-  reloadOwners() {
-    this.service.getAll()
-      .subscribe({
-        next: data => {
-          this.owners = data;
-        },
-        error: error => {
-          console.error('Error fetching owners', error);
-          const errorMessage = error.status === 0
-            ? 'Is the backend up?'
-            : error.message.message;
 
-          this.notification.error(errorMessage, 'Could Not Fetch Owners');
-        }
-      });
+  reloadOwners() {
+    this.service.getAll().subscribe({
+      next: (data) => {
+        this.owners = data;
+      },
+      error: (error) => {
+        console.error('Error fetching owners', error);
+        this.errorHandler(error);
+      },
+    });
+  }
+
+  private errorHandler(
+    error: any,
+    action: string | undefined = undefined
+  ): void {
+    if (error.status === 0) {
+      this.notification.error('Backend is not reachable');
+    } else if (error.status === 404) {
+      this.notification.error(
+        `Could not ${action} horse - ` + error.error.error
+      );
+    } else if (error.status === 409 || error.status === 422) {
+      for (const err of error.error.errors) {
+        this.notification.error(err);
+      }
+    } else if (error.status >= 500) {
+      this.notification.error(
+        error.error.error + ': Something unexpected happend!'
+      );
+    }
   }
 }
